61910efc2490354da463699c76e656b37d5f4115
ryanadams <rcoleman.adams@gmail.com>
2022-04-06 10:44:01 -0500

    commit tree 564f586fc4079bf92e202451e54c7a72745aa101

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+function superbowlWin(array){
+  const result = array.find( ({result})=>result === "W")
+  if(result){
+    return Object.values(result)[0]
+  };
+};
+// console.log(superbowlWin(record))
+// const superbowlWins = record.find( ({result})=>result === "W")
+
+// console.log(superbowlWins.result)
